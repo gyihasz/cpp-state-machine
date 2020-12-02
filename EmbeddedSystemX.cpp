55ee26f61fabf093fe60f997e44ae8565fb0a7a4
domenix <kd.dominik@gmail.com>
2020-12-02 21:11:21 +0100

    Fixed template function and state machine

--- EmbeddedSystemX.cpp
+++ EmbeddedSystemX.cpp
@@ -3,13 +3,7 @@
 #include "Mode2.h"
 #include "Mode3.h"
 #include <iostream>
-//#include "Util.h"
-
-template<typename Base, typename T>
-static inline bool instanceof(const T*) {
-	return std::is_base_of<Base, T>::value;
-}
-
+#include "Util.h"
 
 EmbeddedSystemX::EmbeddedSystemX() {
 	//This is the starting state
@@ -23,42 +17,37 @@ void EmbeddedSystemX::setState(State* state) {
 
 void EmbeddedSystemX::Handle(Event event)
 {
-	//if (instanceof<Mode1>(_state)) {
-
-	if (dynamic_cast<Mode1*>(_state) == nullptr)
+	if (instanceof<Mode1>(_state))
 	{
-		std::cout << "Not in mode1" << std::endl;
+		std::cout << "In mode1" << std::endl;
+		if (event == EVENT_X) {
+			std::cout << "Event X" << std::endl;
+		}
+		else if (event == EVENT_Y) {
+			std::cout << "Event Y" << std::endl;
+		}
 	}
-	if (dynamic_cast<Mode2*>(_state) == nullptr)
+	if (instanceof<Mode2>(_state))
 	{
-		std::cout << "Not in mode2" << std::endl;
+		std::cout << "In mode2" << std::endl;
+		if (event == EVENT_X) {
+			std::cout << "Event X" << std::endl;
+		}
+		else if (event == EVENT_Y) {
+			std::cout << "Event Y" << std::endl;
+		}
 	}
-	if (dynamic_cast<Mode3*>(_state) == nullptr)
+	if (instanceof<Mode3>(_state))
 	{
-		std::cout << "Not in mode3" << std::endl;
+		std::cout << "In mode3" << std::endl;
+		if (event == EVENT_X) {
+			std::cout << "Event X" << std::endl;
+		}
+		else if (event == EVENT_Y) {
+			std::cout << "Event Y" << std::endl;
+		}
 	}
 
-	////is_base_of<Mode1, state>::value;
-	//if (instanceof<Mode2>(_state)) {
-	//	std::cout << "It is in m"
-	//	if (event == EVENT_X) {
-	//		std::cout << "Valamit csinál.";
-	//	}
-	//	else if (event == EVENT_Y) {
-	//		std::cout << "Valamit mást csinál.";
-	//	}
-	//}
-
-	//if (instanceof<Mode2>(_state)) {
-	//	if (event == EVENT_X) {
-	//		std::cout << "Valamit csinál.";
-	//	}
-	//	else if (event == EVENT_Y) {
-	//		std::cout << "Valamit mást csinál.";
-	//	}
-	//}
-
-	std::cout << event << std::endl;
 	_state->Handle();
 }
 
--- Util.h
+++ Util.h
@@ -1,5 +1,8 @@
 #ifndef UtilH
 #define UtilH
 
-
+template<typename T>
+static inline bool instanceof(State* state) {
+	return dynamic_cast<T*>(state) != nullptr;
+}
 #endif
